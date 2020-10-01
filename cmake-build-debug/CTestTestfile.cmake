b4ca5b4e191dd9f4c4c34a0344765f825de57cfe
nishaque <timoshenkojulie01@gmail.com>
2020-10-01 15:17:52 +0300

    update3

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/nishaque/Desktop/33/С++/lab-01-parser
-# Build directory: /Users/nishaque/Desktop/33/С++/lab-01-parser/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unit_tests "/Users/nishaque/Desktop/33/С++/lab-01-parser/cmake-build-debug/tests")
-set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/Users/nishaque/Desktop/33/С++/lab-01-parser/CMakeLists.txt;67;add_test;/Users/nishaque/Desktop/33/С++/lab-01-parser/CMakeLists.txt;0;")
