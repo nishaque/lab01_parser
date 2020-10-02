525eac73f633170effdc5f29b2704bdbea610f29
nishaque <timoshenkojulie01@gmail.com>
2020-10-02 13:04:01 +0300

    without file

--- tests/test.cpp
+++ tests/test.cpp
@@ -95,40 +95,40 @@ TEST(stVect, Vector) {
   studVector s(data);
   ASSERT_EQ(s.st[1].Name, "Sidorov Ivan");
 }
-// TEST(stVect, IncorrectNum) {
-//  json data = (json::parse(R"(
-//      {
-//          "items": [
-//      {
-//        "name": "Ivanov Petr",
-//            "group": "1",
-//            "avg": 4.25,
-//            "debt": null
-//      },
-//      {
-//        "name": "Sidorov Ivan",
-//            "group": 31,
-//            "avg": 4,
-//            "debt": "C++"
-//      },
-//      {
-//        "name": "Pertov Nikita",
-//            "group": "IU8-31",
-//            "avg": 3.33,
-//            "debt": [
-//        "C++",
-//            "Linux",
-//            "Network"
-//        ]
-//      }
-//      ],
-//      "_meta": {
-//        "count": 4
-//      }
-//      }
-//      )"));
-//  ASSERT_THROW(studVector s(data), std::runtime_error);
-//}
-TEST(stVect, correctInputFile) {
-  ASSERT_THROW(studVector s("lab-01-parser/sources/json"), std::runtime_error);
+ TEST(stVect, IncorrectNum) {
+  json data = (json::parse(R"(
+      {
+          "items": [
+      {
+        "name": "Ivanov Petr",
+            "group": "1",
+            "avg": 4.25,
+            "debt": null
+      },
+      {
+        "name": "Sidorov Ivan",
+            "group": 31,
+            "avg": 4,
+            "debt": "C++"
+      },
+      {
+        "name": "Pertov Nikita",
+            "group": "IU8-31",
+            "avg": 3.33,
+            "debt": [
+        "C++",
+            "Linux",
+            "Network"
+        ]
+      }
+      ],
+      "_meta": {
+        "count": 4
+      }
+      }
+      )"));
+  ASSERT_THROW(studVector s(data), std::runtime_error);
 }
+//TEST(stVect, correctInputFile) {
+//  ASSERT_THROW(studVector s("lab-01-parser/sources/json"), std::runtime_error);
+//}
